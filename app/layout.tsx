18f5ab10d2cde5b54e73fc554bf1b5a3297e5ebc
codewithmujab <saefulmujab300@gmail.com>
2024-11-30 21:54:18 +0700

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import "@/app/ui/global.css"; //tambahkan untuk meload global.css
+import { inter } from "@/app/ui/fonts"; // load font
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
